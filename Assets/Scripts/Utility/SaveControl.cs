86136d87c8416a68778438c7b012b6e42c60bb6e
N_Daiki <daiki829mail@gmail.com>
2019-07-19 14:27:50 +0900

    修復用

--- Assets/Scripts/Utility/SaveControl.cs
+++ Assets/Scripts/Utility/SaveControl.cs
@@ -20,7 +20,20 @@ namespace Utility
         /// <typeparam name="T"></typeparam>
         public static void Save<T>(T instance)
         {
+            // ファイルが存在していなかったら作成
+            if (!File.Exists(saveDataAtFilePathConst))
+            {
+                File.Create(saveDataAtFilePathConst);
+            }
+            // Jsonに変換
             string json = JsonUtility.ToJson(instance);
+
+            // 書き込み
+            File.WriteAllText(saveDataAtFilePathConst, json);
+
+#if UNITY_EDITOR
+            Debug.Log(saveDataAtFilePathConst + "にデータを保存しました");
+#endif
         }
 
         /// <summary>
@@ -30,8 +43,18 @@ namespace Utility
         /// <returns></returns>
         public static T Load<T>()
         {
-            string json = File.ReadAllText(saveDataAtFilePathConst);
-            return JsonUtility.FromJson<T>(json);
+            if (!File.Exists(saveDataAtFilePathConst))
+            {
+#if UNITY_EDITOR
+                Debug.Log("ファイルが存在しません");
+#endif
+                return JsonUtility.FromJson<T>(null);
+            }
+            else
+            {
+                string json = File.ReadAllText(saveDataAtFilePathConst);
+                return JsonUtility.FromJson<T>(json);
+            }
         }
     }
 }
